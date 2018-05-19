5b8700fcedcc01a0a3fc5618f88ace5432b590f8
varesc <varesc@gmail.com>
2018-05-19 16:11:00 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express(); // create server
+const hbs = require('hbs');
+const fs = require('fs');
+
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n', (err) =>{
+        if(err){
+            console.log("unable to append file");
+        }
+    });
+
+    next();
+}); // Middleware
+
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs',{
+        title:"Home Page",
+        welcome_message: "Welcome Home",
+    });
+
+});
+
+app.use(express.static(__dirname + '/public')); // Middleware
+
+app.get('/', (req,res) => {
+    // res.send('<h1>Hello Express !</h1>');
+
+    res.render('about.hbs',{
+        title:"Home Page",
+        welcome_message: "Welcome Home",
+    });
+});
+
+
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+        title:"About Page",
+    });
+});
+
+app.get('/bad',(req,res) => {
+   res.send({
+       errorMessage:'Unable to handle request',
+   });
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server is up on port 3000");
+}); // bind app to port (will never stop)
+
+
