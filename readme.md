89f5d67d51201bb89604debba030150921d84dfe
varesc <varesc@gmail.com>
2018-05-20 08:59:43 +0700

    add heroku integration

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # This is a Sample app that I used to learn Node JS
+
+##libaries use in this APP
+
+- Node.JS
+- Express.js
+- Handlebars.js
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // create server
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 app.set('view engine','hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -61,8 +61,8 @@ app.get('/bad',(req,res) => {
 });
 
 
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000");
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
 }); // bind app to port (will never stop)
 
 
